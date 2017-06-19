94e3eec946223b10470c8cced43969c7a7ed929d
farewell1994 <andresk1994@ukr.net>
2017-06-19 12:15:38 +0300

    simple commit

--- app/admin/News.php
+++ app/admin/News.php
@@ -14,5 +14,5 @@ Admin::model(App\News::class)->title('Новини')->with()->filters(function (
 	FormItem::text('small_img', 'Посилання на картинку для представлення новини')->required();
 	FormItem::textarea('description', 'Короткий опис. Відобразиться максимум 200 символів.')->required();
 	FormItem::ckeditor('content', 'Контент. Для коректного відображення картинок на різних пристроях, видаліть їх розміри (які підставляються автоматично) у відповідних полях, та залиште їх незаповненими')->required();
-	FormItem::textarea('links', 'Посилання на 4 фото (розділяти символом *). Наприклад: image1.jpg*image2.jpg*image3.jpg. Не обов\'язковий розділ');
+	FormItem::textarea('links', 'Посилання на фото (розділяти символом *). Наприклад: image1.jpg*image2.jpg*image3.jpg. Не обов\'язковий розділ');
 });
